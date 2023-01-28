93c9b532ed907458f6cdab6a624d92e77f1c592c
Alisson Bergamin <alisson.bergs@gmail.com>
2023-01-28 07:13:21 -0300

    Starting to implement Board and Piece

---
+++ src/main/java/boardgame/Piece.java
@@ -0,0 +1,18 @@
+package boardgame;
+
+public class Piece {
+
+    protected Position position;
+    private Board board;
+
+    public Board getBoard() {
+        return board;
+    }
+
+    protected Piece(Board board) {
+        this.board = board;
+    }
+
+
+
+}
